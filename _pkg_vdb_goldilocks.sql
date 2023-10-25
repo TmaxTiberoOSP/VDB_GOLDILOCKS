4a18ccd963b6063bb7136aaf3176c4e46a030a23
TmaxTiberoOSP <147782683+TmaxTiberoOSP@users.noreply.github.com>
2023-10-25 12:12:01 +0900

    system table 생성문, package spec 생성문, package body 생성문 추가
    create_vdb.sql: system table 생성문, pkg_vdb_goldilocks.sql: package spec 생성문, _pkg_vdb_goldilocks.sql: package body 생성문

---
+++ _pkg_vdb_goldilocks.sql
@@ -0,0 +1,53 @@
+CREATE OR REPLACE PACKAGE BODY "VDB_GOLDILOCKS"
+IS
+    ERROR_PKG_VDB_GOLDILOCKS_DDL_FAIL CONSTANT PLS_INTEGER := -14356;
+
+    PROCEDURE "RAISE_PARAMETER_ERROR" (
+        ERROR_CODE      IN PLS_INTEGER,
+        ERROR_STR       IN VARCHAR2)
+    PRAGMA BUILTIN ('pkg_vdb_goldilocks__raise_parameter_error');
+
+    PROCEDURE "GET_CONNECTION_INFO"(
+        VDB_IP OUT VARCHAR2,
+        VDB_PORT OUT VARCHAR2,
+        ID OUT VARCHAR2,
+        PASSWORD OUT VARCHAR2)
+    PRAGMA BUILTIN('pkg_vdb_goldilocks__get_connection_info');
+    
+    FUNCTION "EXECUTE_DDL_JEPA"(
+        DDL IN VARCHAR2,
+        VDB_IP IN VARCHAR2,
+        VDB_PORT IN VARCHAR2,
+        REMOTE_ID IN VARCHAR2,
+        REMOTE_PASSWD IN VARCHAR2
+    ) RETURN VARCHAR2 IS
+    LANGUAGE JAVA NAME 'ExecuteDDLJDBC.ExecuteDDL(String, String, String, String, String) return String';
+
+    PROCEDURE "MANAGE_REMOTE_OBJECT" (
+      DDL IN VARCHAR2)
+    PRAGMA BUILTIN ('pkg_vdb_goldilocks__manage_remote_object');
+
+    PROCEDURE "EXECUTE_DDL"(DDL IN VARCHAR2)
+    IS
+        VDB_IP          VARCHAR2(20);
+        VDB_PORT        VARCHAR2(20);
+        ID              VARCHAR2(100);
+        PASSWD          VARCHAR2(100);
+        LINK_NAME       VARCHAR2(100);
+        ERR_MSG         VARCHAR2(100);
+    BEGIN
+        COMMIT;
+        /* SELECT 해서 가져오기 */
+        GET_CONNECTION_INFO(VDB_IP, VDB_PORT, ID, PASSWD);
+        MANAGE_REMOTE_OBJECT(DDL);
+        ERR_MSG := EXECUTE_DDL_JEPA(DDL, VDB_IP, VDB_PORT, ID, PASSWD);
+
+        IF ERR_MSG = 'DDL SUCCESS' THEN
+            COMMIT;
+        ELSE
+            ROLLBACK;
+            RAISE_PARAMETER_ERROR(ERROR_PKG_VDB_GOLDILOCKS_DDL_FAIL, ERR_MSG);
+        END IF;
+    END;
+END;
+/
---
+++ create_vdb.sql
@@ -0,0 +1,97 @@
+/**
+ * @file    create_vdb.sql
+ * @brief   Creating the system generated sequences
+ *
+ * @author  kwangwon_park
+ * @version $Id$
+ */
+
+CREATE TABLE _VDB_CONN_INFO (
+    VDB_IP      VARCHAR2(20) PRIMARY KEY,
+    VDB_PORT    VARCHAR2(20),
+    ID          VARCHAR2(100),
+    PASSWORD    VARCHAR2(100)
+);
+
+CREATE TABLE _VDB_OBJECTS(
+	OWNER_ID				VARCHAR2(128),
+	REMOTE_OBJECT_OWNER		VARCHAR2(128),
+	REMOTE_OBJECT_NAME		VARCHAR2(128),
+	REMOTE_OBJECT_TYPE		VARCHAR2(128)
+);
+
+CREATE OR REPLACE AND RESOLVE Java SOURCE NAMED "EXECUTE_DDL_JDBC"
+AS
+import java.sql.*;
+import java.io.*;
+
+public class ExecuteDDLJDBC {
+    public static String ExecuteDDL(String ddl, String ip, String port,
+            String id, String passwd) throws Exception {
+		try
+		{
+			String url = "jdbc:goldilocks://" + ip + ":" + port + "/sundb";
+
+			Connection conn = DriverManager.getConnection(url, id, passwd);
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement(ddl);
+			if(pstmt.executeUpdate() == 0) {
+				conn.commit();
+				pstmt.close();
+				conn.close();
+				return "DDL SUCCESS";
+			}
+
+			conn.rollback();
+			pstmt.close();
+			conn.close();
+			return "Input query is not DDL";
+		}
+		catch (Exception e) {
+			String errMsg = e.getMessage();
+			switch (errMsg) {
+				case "The statement has produced a ResultSet":
+					return "Input query is not DDL";
+				default:
+					return errMsg.substring(0, Math.min(100, errMsg.length()));
+			}
+		}
+	}
+}
+/
+
+CREATE OR REPLACE PUBLIC SYNONYM EXECUTE_DDL_JDBC FOR EXECUTE_DDL_JDBC;
+
+CREATE OR REPLACE VIEW SYSCAT.DBA_VDB_OBJECTS
+(OWNER, REMOTE_OBJECT_OWNER, REMOTE_OBJECT_NAME, REMOTE_OBJECT_TYPE)
+AS
+SELECT U.NAME, V.REMOTE_OBJECT_OWNER, V.REMOTE_OBJECT_NAME, V.REMOTE_OBJECT_TYPE
+FROM SYS._DD_USER U, SYS._VDB_OBJECTS V
+WHERE U.USER_ID = V.OWNER_ID
+;
+
+CREATE OR REPLACE VIEW SYSCAT.ALL_VDB_OBJECTS
+AS
+SELECT * FROM SYSCAT.DBA_VDB_OBJECTS
+WHERE OWNER = USERENV('SCHEMA')
+;
+
+CREATE OR REPLACE VIEW SYSCAT.USER_VDB_OBJECTS
+AS
+SELECT * FROM SYSCAT.DBA_VDB_OBJECTS
+WHERE OWNER = USERENV('SCHEMA')
+;
+
+CREATE OR REPLACE PUBLIC SYNONYM DBA_VDB_OBJECTS
+FOR SYSCAT.DBA_VDB_OBJECTS;
+CREATE OR REPLACE PUBLIC SYNONYM ALL_VDB_OBJECTS
+FOR SYSCAT.ALL_VDB_OBJECTS;
+CREATE OR REPLACE PUBLIC SYNONYM USER_VDB_OBJECTS
+FOR SYSCAT.USER_VDB_OBJECTS;
+
+CREATE OR REPLACE VIEW SYSCAT.VDB_CONN_INFO (VDB_IP, VDB_PORT, ID, PASSWORD)
+AS
+SELECT VDB_IP, VDB_PORT, ID, PASSWORD
+FROM SYS._VDB_CONN_INFO;
+
+CREATE OR REPLACE PUBLIC SYNONYM VDB_CONN_INFO FOR SYSCAT.VDB_CONN_INFO;
